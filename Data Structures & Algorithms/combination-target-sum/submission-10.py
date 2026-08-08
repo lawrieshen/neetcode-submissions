69e7a0c076541a055b04ff2c207d4c402df696c3
Lawrence Shen <102568086+lawrieshen@users.noreply.github.com>
2026-08-08 22:31:38 +1000

    Add: combination-target-sum - submission-10

---
+++ Data Structures & Algorithms/combination-target-sum/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(start, path, total):
+            if total == target:
+                res.append(path[:])
+
+            for i in range(start, len(nums)):
+                if total + nums[i] > target:
+                    return
+                path.append(nums[i])
+                backtrack(i, path, total + nums[i])
+                path.pop()
+
+        backtrack(0, [], 0)
+        return res
+
+        # Time: O(N^(t/m)           * (t/m))
+        #.      遞迴數最多產生的節點數   複製path的最大長度
+        # Space: O(t/m) 
+        # where t us the given target and m is min(nums)
